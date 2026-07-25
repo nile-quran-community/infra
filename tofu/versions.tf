680a4077af19fda18dc37ea4abac037840f73022
ibrahimhefny <ibrahim.mo.hefny@gmail.com>
2026-07-25 23:47:17 +0300

    chore(tofu): remove unused tls/random/local providers

--- tofu/versions.tf
+++ tofu/versions.tf
@@ -1,5 +1,3 @@
-# Configure the required OpenTofu version and provider dependencies.
-
 terraform {
   required_version = ">= 1.6"
 
@@ -7,21 +5,6 @@ terraform {
     hostinger = {
       source  = "hostinger/hostinger"
       version = "~> 0.1.22"
-    }
-
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
-
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.5"
     }
   }
 }
