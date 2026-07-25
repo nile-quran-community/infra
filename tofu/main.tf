c1b5230159313655a65fc69869813cfaca36db0c
ibrahimhefny <ibrahim.mo.hefny@gmail.com>
2026-07-25 23:48:09 +0300

    fix(tofu): attach all configured SSH keys, use k3s post-install script

--- tofu/main.tf
+++ tofu/main.tf
@@ -1,14 +1,3 @@
-# Generate a backup password for each VPS.
-# This can be used if password authentication is required.
-resource "random_password" "vps" {
-  for_each = var.vps_instances
-
-  length           = 20
-  special          = true
-  override_special = "!@#%^*()-_=+"
-}
-
-# Create one VPS for each entry in vps_instances.
 resource "hostinger_vps" "server" {
   for_each = var.vps_instances
 
@@ -17,14 +6,8 @@ resource "hostinger_vps" "server" {
   template_id    = each.value.template_id
   data_center_id = each.value.data_center_id
 
-  # Configure SSH access using the uploaded key.
-  ssh_key_ids = [
-    hostinger_vps_ssh_key.main.id
-  ]
+  # Attach every configured SSH key to every VPS.
+  ssh_key_ids = [for k in hostinger_vps_ssh_key.main : k.id]
 
-  # Uncomment if the provider requires a password.
-  # password = random_password.vps[each.key].result
-
-  # Run the bootstrap script after provisioning.
-  post_install_script_id = var.install_docker ? hostinger_vps_post_install_script.docker_setup[0].id : null
+  post_install_script_id = hostinger_vps_post_install_script.k3s_setup.id
 }
